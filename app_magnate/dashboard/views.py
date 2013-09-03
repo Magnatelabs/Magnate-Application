0dc7073919f26aa307efb325d04efc5c2447fc46
jmwilliams(air) <jolumwilliams@gmail.com>
2013-09-02 20:40:19 -0400

    dashboard loginview fix

--- app_magnate/dashboard/views.py
+++ app_magnate/dashboard/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views import generic
 from django.utils import timezone
 
 def dashboard_index(request):
+    if not request.user.is_authenticated():
+        return redirect('/account/login/?next=%s' % request.path)
+
     return render(request, 'dashboard/dashboard_main.html')
+
