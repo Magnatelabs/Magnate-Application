6954b8e6139ed25626ab8c6d0c86e38fe343cedb
jmwilliams(air) <jolumwilliams@gmail.com>
2013-11-25 00:12:13 -0500

    added all_donations_by_user that returns the list of all donations

--- app_magnate/donations/utils.py
+++ app_magnate/donations/utils.py
@@ -4,3 +4,6 @@ from .models import Donation
 # Returns a Decimal
 def total_donation_amount(username):
     return sum([d.amount for d in Donation.objects.filter(user__username=username)])
+
+def all_donations_by_user(username):
+    return Donation.objects.filter(user__username=username)
