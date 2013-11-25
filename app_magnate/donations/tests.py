1901407bbebf1ff9773fe87ba60ca82b325e8e5c
jmwilliams(air) <jolumwilliams@gmail.com>
2013-11-24 21:11:19 -0500

    added test cases for the donations application--they run fine

--- app_magnate/donations/tests.py
+++ app_magnate/donations/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+class DonationTestCase(TestCase):
+    def test_successfull_donation(self):
+        """Authorize.net donation went through"""
+        print "hello went through"
+
+    def test_failed_donation(self):
+        """Authorize.net donation did not go through"""
+        print "sucks bro"
+
