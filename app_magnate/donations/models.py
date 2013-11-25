eda4fa435a1a73f0094e57bdbf5e57cc3a1cbea2
jmwilliams(air) <jolumwilliams@gmail.com>
2013-11-24 22:56:39 -0500

    Added models Donation and FailedDonation. Also added signal handlers on_transaction_was_successful and on_transaction_was_unsuccessful to handle the result of payment processing. Signals are sent from billing/integrations/AuthorizeNetDPMIntegration

--- app_magnate/donations/models.py
+++ app_magnate/donations/models.py
@@ -3,6 +3,11 @@ from django.utils import timezone
 from django import forms
 from django.contrib.auth.models import User
 
+from billing.signals import transaction_was_successful, transaction_was_unsuccessful
+from django.dispatch import receiver
+
+import datetime
+
 class BillingInfo (models.Model):
     user = models.ForeignKey(User)
     first_name = models.CharField(max_length=200)
@@ -15,3 +20,44 @@ class BillingInfo (models.Model):
     def __unicode__(self):
         return self.first_name
 
+class Donation (models.Model):
+    user = models.ForeignKey(User)
+    transaction_id = models.CharField(max_length=64)
+    amount = models.DecimalField(max_digits=16, decimal_places=2)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return '$%.2f by %s on %s' % (self.amount, self.user, self.timestamp)
+
+    @receiver(transaction_was_successful)
+    def on_transaction_was_successful(sender, **kwargs):
+        print 'transaction successful'
+        data = kwargs['request'].POST
+
+        try:
+            user=User.objects.get(username=data['x_cust_id'])
+        except User.DoesNotExist, e:
+            # LOG FATAL ERROR
+            print 'FATAL ERROR while saving a donation: User %s does not exist' % data['x_cust_id']
+            return
+
+        user = User.objects.get(username=data['x_cust_id'])
+        amount = data['x_amount']
+        transaction_id = data['x_trans_id']
+        donation = Donation(user=user, amount=amount, transaction_id=transaction_id)  
+        donation.save()
+        #import pdb; pdb.set_trace()
+
+
+class FailedDonation (models.Model):
+    user = models.ForeignKey(User)
+    amount = models.DecimalField(max_digits=16, decimal_places=2)
+    timestamp = models.DateTimeField()
+
+    def __unicode__(self):
+        return '$%.2f by %s on %s' % (self.amount, self.user, self.timestamp)
+
+    @receiver(transaction_was_unsuccessful)
+    def on_transaction_was_unsuccessful(sender, **kwargs):
+        print 'transaction failed'
+        #import pdb; pdb.set_trace()
