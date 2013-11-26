508751618b81cd7adee210f6239e0d8f5276ff0d
jmwilliams(air) <jolumwilliams@gmail.com>
2013-11-25 23:11:01 -0500

    added all_badges_for_user. So far it is pretty stupid. But it should return a list of badges for the user. Each badge should have as_html

--- app_magnate/donations/utils.py
+++ app_magnate/donations/utils.py
@@ -1,4 +1,5 @@
 from .models import Donation
+from django.conf import settings
 
 # username is a string, such as 'admin'
 # Returns a Decimal
@@ -7,3 +8,10 @@ def total_donation_amount(username):
 
 def all_donations_by_user(username):
     return Donation.objects.filter(user__username=username)
+
+def all_badges_for_user(username):
+    l=  [{'as_html': "<img src=\"%s\" alt=\"%s\" />" % (settings.STATIC_URL + 'img/img_icon13.png', 'BADGE')}, {'as_html': "<img src=\"%s\" alt=\"%s\" />" % (settings.STATIC_URL + 'img/img_icon14.png', 'BADGE')}, {'as_html': "<img src=\"%s\" alt=\"%s\" />" % (settings.STATIC_URL + 'img/img_icon15.png', 'BADGE')}]
+    if all_donations_by_user(username)>1.0:
+        return l+l+l
+    else:
+        return []
