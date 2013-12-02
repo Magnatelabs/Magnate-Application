890d9cf01af804893ebf6f70c656a268792ec271
jmwilliams(air) <jolumwilliams@gmail.com>
2013-12-02 17:56:05 -0500

    Dashboard is now a class-based view DashboardView.as_view()

--- app_magnate/dashboard/urls.py
+++ app_magnate/dashboard/urls.py
@@ -4,12 +4,13 @@ from dashboard import views
 
 #for the FormView
 from dashboard.views import dashboard_spotlight_request 
+from dashboard.views import DashboardView
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns("",
-  url(r"^dashboard/$", "dashboard.views.dashboard_index", name="dashboard"),
+  url(r"^dashboard/$", DashboardView.as_view(), name="dashboard"),
   url(r"^dashboard/spotlightrequest/$", dashboard_spotlight_request.as_view(), name="spotlight_request"),
 url(r"^spotlight_confirm/$", "dashboard.views.dash_confirm_index", name="confirm_dashboard"),
 #  url(r"^dashboard/spotlightrequest/$", "dashboard.views.dashboard_spotlight_request", name="spotlight_request"),
