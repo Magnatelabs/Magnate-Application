3c0bcc69294b9d4b21ebc352b9e7895595c07956
jmwilliams(air) <jolumwilliams@gmail.com>
2013-12-02 01:00:29 -0500

    adding zinnia to magnate

--- app_magnate/app_magnate/urls.py
+++ app_magnate/app_magnate/urls.py
@@ -3,6 +3,9 @@ from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
+from zinnia.views.archives import EntryIndex
+
+
 from django.contrib import admin
 admin.autodiscover()
 
@@ -12,6 +15,32 @@ urlpatterns = patterns("",
     url(r"^admin/", include(admin.site.urls)),
     url(r"^account/", include("account.urls")),
     url(r"^waitinglist/", include("waitinglist.urls")),
+
+# We don't want to include all Zinnia.urls, as we do not want to expose
+# everything to the users. It may be possible to e.g. decorate the include
+# with login_required, see http://stackoverflow.com/questions/2307926/is-it-possible-to-decorate-include-in-django-urls-with-login-required
+# I guess, it may even be possible to make zinnia.urls available
+# only for certain users. For now we want them closed.
+    url(r'^blog/', include('zinnia.urls')),
+#    url(r'^blg/', EntryIndex.as_view()),
+
+# NOT SURE about this one... Needed for Zinnia...    
+    url(r'^comments/', include('django.contrib.comments.urls')),
+
+# However, then admin cannot work with Zinnia entries. It complains
+# about NoReverseMatch, because it is trying to display a list of URLs
+# linking to the posts on the website... So here we are faking it
+# by allowing it to actually generate links, though those links
+# won't lead to anywhere. (Double-check when upgrading Zinia 
+# that it doesn't create any security holes, though it shouldn't).
+#
+# TODO: perhaps just remove the URL column from the Zinnia Entry in
+# admin... 
+# TODO: ideally, include zinnia.urls, but decorate them somehow so that
+# only root (or certain users) would be able to view them. This would
+# be nice for the admin - to see all posts, etc.
+    url(r'^zinnia_shortlink/', include('zinnia.urls.shortlink')),
+
 #    url(r"^contacts/", include("phonebook.urls")),
 #    url(r"^testform/", include("testFormApp.urls")),
     url(r"^startedsurvey/", include("getstartedquestions.urls")), 
