c99f84cda0adddec5b8f78fd15e809cb735fd704
jmwilliams(air) <jolumwilliams@gmail.com>
2013-12-03 15:12:30 -0500

    added functions total_entry_likes(entry) to get the total number of likes for a given Zinnia entry and toggle_like_unlike(entry, user) to like/unlike entries. The latter also returns (?liked, total_likes)

--- app_magnate/social/models.py
+++ app_magnate/social/models.py
@@ -10,6 +10,10 @@ class Like(models.Model):
 
     # returns (Boolean, Int) - whether the user likes the entry and the total number of likes(=liking users) the entry has
 
+def total_entry_likes(entry):
+    #    return Like.objects.filter(entry=entry).count()
+    return sum([l.total_likes for l in Like.objects.filter(entry=entry)])
+
 def entry_is_liked(entry, user):
     liked = Like.objects.create(entry=entry)
     try:
@@ -20,3 +24,23 @@ def entry_is_liked(entry, user):
         return (True, user_liked.total_likes)
     else:
         return (False, liked.total_likes)
+
+
+# If the user likes the entry, now it will be unliked, and vice versa.
+# Returns (bool, int), i.e. liked = Like.objects.create(entry=entry)
+def toggle_like_unlike(entry, user):
+        liked = Like.objects.create(entry=entry)
+        try:
+            user_liked = Like.objects.get(entry=entry, user=user)
+        except:
+            user_liked=None
+        if user_liked:  # If the "Like" object already existed. (We did not save the new one yet.)
+            user_liked.total_likes -= 1
+            user_liked.user.remove(user) 
+            user_liked.save() # Decreased the number of likes by one. Second "Like" is interpreted as "Unlike"
+            return (False, user_liked.total_likes)
+        else: # It's a new "Like", not an "Unlike"
+            liked.user.add(user)
+            liked.total_likes += 1
+            liked.save()
+            return (True, liked.total_likes)
