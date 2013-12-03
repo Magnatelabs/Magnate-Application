19aa860d8ed8a90acde5c2ebf1fd3171572d7107
jmwilliams(air) <jolumwilliams@gmail.com>
2013-12-02 19:44:52 -0500

    Added class AuthorizedUserMixin, class AuthorizedUserQuerySet, AuthorizedEntriesManager.   This is all to add the function authorized_or_published to AuthorizedEntriesManager. Our extension of Zinnia's entry model is using this manager: class glue_zinnia.models.EntryCheck sets private=AuthorizedEntriesManager(). This way we can write e.g. zinnia.models.Entry.private.authorized_or_published(self.request.user)

--- app_magnate/glue_zinnia/models.py
+++ app_magnate/glue_zinnia/models.py
@@ -2,7 +2,48 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.utils.translation import ugettext_lazy as _
 from zinnia.models.entry import EntryAbstractClass, CoreEntry
+from zinnia.managers import PUBLISHED
 
+
+from django.db.models.query import QuerySet
+from django.db.models import Q
+
+# From  http://stackoverflow.com/questions/2163151/custom-queryset-and-manager-without-breaking-dry
+#
+# Now we can write things like zinnia.models.Entry.private.by_authorized_user(self.request.user)
+# to get the list of all entries where the current user is included in the list of authorized_users.
+#
+# Beware: zinnia.models.Entry.private.by_authorized_user(None) will return all entries.
+# Thus make sure that somebody is authorized!
+
+class AuthorizedUserMixin(object):
+    # Return all entries that are either authorized for a particular user OR published (or both, of course, but we don't expect anyone to set authorized_users for a published entry. All entries meant only for particular users should be HIDDEN.).
+    # @user has type django.contrib.auth.get_user_model()
+    def authorized_or_published(self, user):
+        # Make sure that we are given a real user.
+        # Otherwise we risk returning ALL entries, including all hidden ones, instead of those authorized for a particular user.
+        assert user.is_authenticated(), "Yes" 
+
+        # FIXME! TODO! Temporary assertions. Can remove if everything is fine.
+        for entry in self.filter(authorized_users=user):
+            assert user in entry.authorized_users.all(), "Tried to return an entry not authorized for %s" % (str(user))
+
+        return self.filter(Q(authorized_users=user) | Q(status=PUBLISHED))
+
+class AuthorizedUserQuerySet(QuerySet, AuthorizedUserMixin):
+    pass
+
+
+class AuthorizedEntriesManager(models.Manager, AuthorizedUserMixin):
+    def get_queryset(self):
+        return AuthorizedUserQuerySet(self.model, using=self._db)
+#    def get_queryset(self):
+#        return super(AuthorizedEntriesManager, self).get_queryset().filter(status==HIDDEN)
+
+    # Retrieve those Zinnia entries where the list of authorized_users includes user.
+    # (Parameter "user" has type get_user_model())
+    def get_user_queryset(self, user):
+        return self.get_queryset().filter(authorized_users=user)
 
 class AuthorizedUsersEntry(models.Model):
     """
@@ -30,6 +71,8 @@ class EntryCheck(
 
     def __unicode__(self):
         return u'EntryCheck %s' % self.title
+
+    private = AuthorizedEntriesManager()
 
     class Meta(CoreEntry.Meta):
         abstract = True
