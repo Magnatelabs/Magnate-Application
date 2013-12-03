f522c6371ba50694b3e791d714cd31581ff1286a
jmwilliams(air) <jolumwilliams@gmail.com>
2013-12-03 15:12:44 -0500

    simple test to check likes/unlikes

--- app_magnate/social/tests.py
+++ app_magnate/social/tests.py
@@ -1,16 +1,31 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 
+from django.contrib.auth import get_user_model
+User=get_user_model()
+from zinnia.models.entry import Entry
+from . import models
+from models import total_entry_likes
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+        user = User.objects.create_user("Monica Schlicht", "m-schlicht@clearwaters.sky", "2380vh23v")
+        entry = Entry.objects.create(title="Who owns Warhol portrait of Fawcett?")
+
+        liked, total = models.entry_is_liked(entry, user)
+        self.assertEqual(liked, False)
+        self.assertEqual(total, 0)
+        self.assertEqual(total_entry_likes(entry), 0)
+
+        # Like and Unlike the entry several times in a row
+        for i in range(5):
+            liked, total = models.toggle_like_unlike(entry, user)
+            # Now the user likes the entry
+            self.assertEqual(liked, True)
+            self.assertEqual(total, 1)
+            self.assertEqual(total_entry_likes(entry), 1)
+
+            liked, total = models.toggle_like_unlike(entry, user)
+            # Now the user unliked the entry, back to where we were
+            self.assertEqual(liked, False)
+            self.assertEqual(total, 0)
+            self.assertEqual(total_entry_likes(entry), 0)
