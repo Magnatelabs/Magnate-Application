e47fe551a5b0df16c065be6afbef7b8c365034df
jmwilliams(air) <jolumwilliams@gmail.com>
2013-12-03 13:38:57 -0500

    Crazy experiment: created a new model AuthorizedEntry independent of the original

--- app_magnate/glue_zinnia/admin.py
+++ app_magnate/glue_zinnia/admin.py
@@ -4,7 +4,16 @@ from django.utils.translation import ugettext_lazy as _
 from zinnia.models.entry import Entry
 from zinnia.admin.entry import EntryAdmin
 
-class EntryCheckAdmin(EntryAdmin):
+from glue_zinnia.models import AuthorizedEntry
+
+
+from zinnia.admin.forms import EntryAdminForm
+class AuthorizedEntryAdminForm(EntryAdminForm):
+    class Meta:
+        model=AuthorizedEntry
+
+class AuthorizedEntryAdmin(EntryAdmin):
+  form=AuthorizedEntryAdminForm
   # In our case we put the gallery field
   # into the 'Content' fieldset
 #  print '!!!'
@@ -16,7 +25,18 @@ class EntryCheckAdmin(EntryAdmin):
     EntryAdmin.fieldsets[3:]
   filter_horizontal = EntryAdmin.filter_horizontal + ('authorized_users',)
 
+  def queryset(self, request):
+        """Make special filtering by user permissions"""
+        if not request.user.has_perm('zinnia.can_view_all'):
+            queryset = request.user.authorizedentry.all() ### changed entries.all() to authorizedentry.all()
+        else:
+            queryset = super(AuthorizedEntryAdmin, self).queryset(request) ### changed EntryAdmin to AuthorizedEntryAdmin
+        return queryset.prefetch_related('categories', 'authors', 'sites')
+
 # Unregister the default EntryAdmin
 # then register the EntryCheckAdmin class
-admin.site.unregister(Entry)
-admin.site.register(Entry, EntryCheckAdmin)
+#admin.site.unregister(Entry)
+#admin.site.register(Entry, EntryCheckAdmin)
+
+
+#admin.site.register(AuthorizedEntry, EntryAdmin)
