7dbce4866dcb5233a333ee2c494dae13a0631c62
jmwilliams(air) <jolumwilliams@gmail.com>
2013-12-03 20:26:13 -0500

    do_many and test_random: more sophisticated testing with random liking/unliking

--- app_magnate/social/tests.py
+++ app_magnate/social/tests.py
@@ -6,6 +6,10 @@ from zinnia.models.entry import Entry
 from . import models
 from models import total_entry_likes, entry_is_liked, toggle_like_unlike
 
+import random
+random.seed(2746833)
+from random import randrange
+
 class SimpleTest(TestCase):
     def test_basic_addition(self):
         user = User.objects.create_user("Monica Schlicht", "m-schlicht@clearwaters.sky", "2380vh23v")
@@ -66,4 +70,28 @@ class SimpleTest(TestCase):
         count = entry_is_liked(entry, u2)
         self.assertEqual(count, 0)
         self.assertEqual(total_entry_likes(entry), 0)
-        
+     
+ 
+    def do_many(self, n_users, n_entries, n_repetitions):
+        users=[]
+        for i in range(n_users):
+            users.append(User.objects.create_user("User %d" % (i), "user-%d@people.biz" % (i), "12345678987654321"))
+        entries=[]
+        for i in range(n_entries):
+            entries.append(Entry.objects.create(title="Entry %d" % (i)))
+        likes=[[False for e in entries] for u in users]
+
+        for r in range(n_repetitions):
+            u_ind=randrange(n_users)
+            user=users[u_ind]
+            e_ind=randrange(n_entries)
+            entry=entries[e_ind]
+            self.assertEqual(entry_is_liked(entry, user), int(likes[u_ind][e_ind]))
+            result=toggle_like_unlike(entry, user)
+            likes[u_ind][e_ind] = not likes[u_ind][e_ind]
+            self.assertEqual(entry_is_liked(entry, user), int(likes[u_ind][e_ind]))
+            self.assertEqual(result, int(likes[u_ind][e_ind]))
+            self.assertEqual(total_entry_likes(entry), len([ui for ui in range(n_users) if likes[ui][e_ind]]))
+
+    def test_random(self):
+        self.do_many(1, 1, 100)
