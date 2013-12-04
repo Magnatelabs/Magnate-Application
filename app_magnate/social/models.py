9dd5ce11d973237be3f72d73180a42105b696312
jmwilliams(air) <jolumwilliams@gmail.com>
2013-12-03 20:02:22 -0500

    Fixed basic like/unlike functionality

--- app_magnate/social/models.py
+++ app_magnate/social/models.py
@@ -2,46 +2,48 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from zinnia.models.entry import Entry
 
+# Every instance will represent one Like of one entry by one user
 class Like(models.Model):
-    user=models.ManyToManyField(get_user_model(), related_name='likes')
+    user=models.ForeignKey(get_user_model(), related_name='likes')
     entry=models.ForeignKey(Entry)
     date=models.DateTimeField(auto_now_add=True)
-    total_likes=models.IntegerField(default=0)
+    count=models.IntegerField(default=0) # This is typically 0/1
 
     # returns (Boolean, Int) - whether the user likes the entry and the total number of likes(=liking users) the entry has
 
 def total_entry_likes(entry):
     #    return Like.objects.filter(entry=entry).count()
-    return sum([l.total_likes for l in Like.objects.filter(entry=entry)])
+    return sum([l.count for l in Like.objects.filter(entry=entry)])
+
+
+    
+
+def entry_liked_count(entry, user):
+    try:
+        like = Like.objects.get(entry=entry, user=user)
+        return like.count
+    except:
+        return 0
 
 def entry_is_liked(entry, user):
-    liked = Like.objects.create(entry=entry)
-    try:
-        user_liked = Like.objects.get(entry=entry, user=user)
-    except:
-        user_liked=None
-    if user_liked:
-        return (True, user_liked.total_likes)
-    else:
-        return (False, total_entry_likes(entry))
-#        return (False, liked.total_likes)
+    return entry_liked_count(entry, user) > 0
+
 
 
 # If the user likes the entry, now it will be unliked, and vice versa.
 # Returns (bool, int), i.e. liked = Like.objects.create(entry=entry)
 def toggle_like_unlike(entry, user):
-        liked = Like.objects.create(entry=entry)
         try:
-            user_liked = Like.objects.get(entry=entry, user=user)
+            like = Like.objects.get(entry=entry, user=user)
         except:
-            user_liked=None
-        if user_liked:  # If the "Like" object already existed. (We did not save the new one yet.)
-            user_liked.total_likes -= 1
-            user_liked.user.remove(user) 
-            user_liked.save() # Decreased the number of likes by one. Second "Like" is interpreted as "Unlike"
-            return (False, user_liked.total_likes)
+            like=None
+        if like: 
+            if (like.count == 0):
+                like.count = 1
+            else:
+                like.count = 0
         else: # It's a new "Like", not an "Unlike"
-            liked.user.add(user)
-            liked.total_likes += 1
-            liked.save()
-            return (True, liked.total_likes)
+            like = Like.objects.create(entry=entry, user=user)
+            like.count = 1
+        like.save()
+        return like.count
--- app_magnate/social/tests.py
+++ app_magnate/social/tests.py
@@ -11,23 +11,19 @@ class SimpleTest(TestCase):
         user = User.objects.create_user("Monica Schlicht", "m-schlicht@clearwaters.sky", "2380vh23v")
         entry = Entry.objects.create(title="Who owns Warhol portrait of Fawcett?")
 
-        liked, total = entry_is_liked(entry, user)
-        self.assertEqual(liked, False)
-        self.assertEqual(total, 0)
+        self.assertEqual(entry_is_liked(entry, user), 0)
         self.assertEqual(total_entry_likes(entry), 0)
 
         # Like and Unlike the entry several times in a row
         for i in range(5):
-            liked, total = toggle_like_unlike(entry, user)
+            count = toggle_like_unlike(entry, user)
             # Now the user likes the entry
-            self.assertEqual(liked, True)
-            self.assertEqual(total, 1)
+            self.assertEqual(count, 1)
             self.assertEqual(total_entry_likes(entry), 1)
 
-            liked, total = toggle_like_unlike(entry, user)
+            count = toggle_like_unlike(entry, user)
             # Now the user unliked the entry, back to where we were
-            self.assertEqual(liked, False)
-            self.assertEqual(total, 0)
+            self.assertEqual(count, 0)
             self.assertEqual(total_entry_likes(entry), 0)
 
 
@@ -35,51 +31,39 @@ class SimpleTest(TestCase):
 
         u2 = User.objects.create_user("Schwarzenegger", "a-r@california.org", "32!!!@FJ#IOJV#E")
         # Now user u2 comes and likes the entry
-        liked, total = toggle_like_unlike(entry, u2)
-        self.assertEqual(liked, True)
-        self.assertEqual(total, 1)
-        liked, total = entry_is_liked(entry, user) # No, user doesn't like it
-        self.assertEqual(liked, False)
-        self.assertEqual(total, 1) # but we see the like of u2
-        liked, total = entry_is_liked(entry, u2) # Yes, u2 likes it
-        self.assertEqual(liked, True)
-        self.assertEqual(total, 1)
-        self.assertEqual(total_entry_likes(entry), 1)
+        count = toggle_like_unlike(entry, u2)
+        self.assertEqual(count, 1)
+        count = entry_is_liked(entry, user) # No, user doesn't like it
+        self.assertEqual(count, 0) 
+        self.assertEqual(total_entry_likes(entry), 1) # but we see the like of u2
+        count = entry_is_liked(entry, u2) # Yes, u2 likes it
+        self.assertEqual(count, 1)
 
 
         # Now user also likes the entry
-        liked, total = toggle_like_unlike(entry, user)
-        self.assertEqual(liked, True)
-        self.assertEqual(total, 2)
-        liked, total = entry_is_liked(entry, user) # Now both like it
-        self.assertEqual(liked, True)
-        self.assertEqual(total, 2)
-        liked, total = entry_is_liked(entry, u2)
-        self.assertEqual(liked, True)
-        self.assertEqual(total, 2)
+        count = toggle_like_unlike(entry, user)
+        self.assertEqual(count, 1)
+        count = entry_is_liked(entry, user) # Now both like it
+        self.assertEqual(count, 1)
+        count = entry_is_liked(entry, u2)
+        self.assertEqual(count, 1)
         self.assertEqual(total_entry_likes(entry), 2)
 
         # Now u2 unlikes the entry
-        liked, total = toggle_like_unlike(entry, u2)
-        self.assertEqual(liked, False)
-        self.assertEqual(total, 1) # user still likes it
-        liked, total = entry_is_liked(entry, user)
-        self.assertEqual(liked, True)
-        self.assertEqual(total, 1)
-        liked, total = entry_is_liked(entry, u2)
-        self.assertEqual(liked, False)
-        self.assertEqual(total, 1)
+        count = toggle_like_unlike(entry, u2)
+        self.assertEqual(count, 0) # user still likes it
+        count = entry_is_liked(entry, user)
+        self.assertEqual(count, 1)
+        count = entry_is_liked(entry, u2)
+        self.assertEqual(count, 0)
         self.assertEqual(total_entry_likes(entry), 1)
 
         # Now user also unlikes the entry
-        liked, total = toggle_like_unlike(entry, user)
-        self.assertEqual(liked, False)
-        self.assertEqual(total, 0)
-        liked, total = entry_is_liked(entry, user)
-        self.assertEqual(liked, False)
-        self.assertEqual(total, 0)
-        liked, total = entry_is_liked(entry, u2)
-        self.assertEqual(liked, False)
-        self.assertEqual(total, 0)
+        count = toggle_like_unlike(entry, user)
+        self.assertEqual(count, 0)
+        count = entry_is_liked(entry, user)
+        self.assertEqual(count, 0)
+        count = entry_is_liked(entry, u2)
+        self.assertEqual(count, 0)
         self.assertEqual(total_entry_likes(entry), 0)
         
