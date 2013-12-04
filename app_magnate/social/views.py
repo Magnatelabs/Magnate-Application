d3833b5c13608cddcd9519c414078ed89d50a4b4
jmwilliams(air) <jolumwilliams@gmail.com>
2013-12-03 23:39:39 -0500

    Fixed ajax_like_entry(request). Replaced most of the code with calls to toggle_like_unlike and total_entry_likes. Also now checking whether request is ajax and whether it is authenticated

--- app_magnate/social/views.py
+++ app_magnate/social/views.py
@@ -2,39 +2,30 @@ from django.http import HttpResponse
 from django.utils import simplejson
 from django.shortcuts import get_object_or_404
 from zinnia.models.entry import Entry
-from .models import Like
+
 from .templatetags import social_tags
+from django.http import HttpResponseBadRequest
+from django.utils import simplejson
+
+from .models import toggle_like_unlike, total_entry_likes
 
 #TODO! FIXME! Think about thread safety if multiple users like the same post at the same time!
 
 # User clicked the "Like" button after a post.
 def ajax_like_entry(request):
+    if not request.is_ajax():
+        return HttpResponseBadRequest
+    if not request.user.is_authenticated():
+        return HttpResponse(status=401)
+
     vars={}
     if request.method=='POST':
         user=request.user
         entry_id=request.POST.get('entry_id', None)
         entry=get_object_or_404(Entry, pk=entry_id)
-        liked = Like.objects.create(entry=entry)
-        try:
-            user_liked = Like.objects.get(entry=entry, user=user)
-        except:
-            user_liked=None
-        if user_liked: # If the "Like" object already existed. (We did not save the new one yet.)
-            user_liked.total_likes -= 1
-            user_liked.user.remove(request.user) 
-            user_liked.save() # Decreased the number of likes by one. Second "Like" is interpreted as "Unlike"
-#            print "Unliked!"
-            vars['liked']=False
-            vars['total_likes']=user_liked.total_likes
-            vars['message']="Thank you for unliking! Now %d likes" % (user_liked.total_likes)
-        else: # It's a new "Like", not an "Unlike"
-            liked.user.add(request.user)
-            liked.total_likes += 1
-            liked.save()
-#            print "Liked!"
-            vars['liked']=True
-            vars['total_likes']=liked.total_likes
-            vars['message']="Thank you for liking! Now %d likes" % (liked.total_likes)
-  
+
+        count = toggle_like_unlike(entry, user)
+        vars['liked'] = (count > 0)
+        vars['total_likes'] = total_entry_likes(entry)
         vars['update_html']={social_tags.get_div_dom_id(entry_id) : social_tags.like_entry_button(int(entry_id), user)}
     return HttpResponse(simplejson.dumps(vars), mimetype='application/javascript')
