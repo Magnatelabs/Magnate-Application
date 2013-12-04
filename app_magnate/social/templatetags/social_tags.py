8e10cae348fe2e4fdcc0ef652a9e9339130a4830
jmwilliams(air) <jolumwilliams@gmail.com>
2013-12-03 21:32:47 -0500

    fix

--- app_magnate/social/templatetags/social_tags.py
+++ app_magnate/social/templatetags/social_tags.py
@@ -1,6 +1,6 @@
 from django import template
 from django.shortcuts import get_object_or_404
-from ..models import Like, entry_is_liked
+from ..models import Like, entry_is_liked, total_entry_likes
 from zinnia.models.entry import Entry
 
 register=template.Library()
@@ -16,7 +16,8 @@ def like_entry_button(entry_id, user): # entry_id is an integer
     dom_id = get_button_dom_id(entry_id)
     div_dom_id = get_div_dom_id(entry_id)
     entry=get_object_or_404(Entry, pk=entry_id)
-    user_likes_entry, total_likes = entry_is_liked(entry, user)
+    user_likes_entry = entry_is_liked(entry, user)
+    total_likes = total_entry_likes(entry) # total number of users who like it
     button_text=['Like it', 'Unlike it'][user_likes_entry] + (' (%d)' % (total_likes))
     return '<div id=%s><input type="Button" id="%s" value="%s" style="float: right" onClick="on_click_like_entry(%d, this.id)" /> </div>' % (div_dom_id, dom_id, button_text, entry_id)
 
