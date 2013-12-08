5a02e948a23abf6ed01b238100dc7ea5e4104704
Jimi Williams <tinaglorbian@Tinas-MacBook-Air.local>
2013-12-08 00:10:43 -0500

    The fields x_city and x_country are now readonly

--- app_magnate/billing/forms/authorize_net_forms.py
+++ app_magnate/billing/forms/authorize_net_forms.py
@@ -10,10 +10,10 @@ class AuthorizeNetDPMForm(forms.Form):
     x_last_name = forms.CharField(max_length=50, label="Last Name")
 
     x_address = forms.CharField(widget=forms.Textarea, max_length=60, label="Address")
-    x_city = forms.CharField(max_length=40, label="City")
+    x_city = forms.CharField(max_length=40, label="City", widget=forms.TextInput(attrs={'readonly':'readonly'}))
     x_state = forms.CharField(max_length=40, label="State")
     x_zip = forms.CharField(max_length=20, label="Zip")
-    x_country = forms.CharField(max_length=60, label="Country")
+    x_country = forms.CharField(max_length=60, label="Country", widget=forms.TextInput(attrs={'readonly':'readonly'}))
 
     x_amount = forms.CharField(label="Amount (in USD)", widget=forms.TextInput(attrs={'readonly':'readonly'}))
 
