41f930698ead3b89ba8bafddba5d99293051410f
jmwilliams(air) <jolumwilliams@gmail.com>
2013-12-09 17:29:08 -0500

    Added a simple test for the redefined BadgeAward model

--- app_magnate/status_awards/tests.py
+++ app_magnate/status_awards/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+User=get_user_model()
 
 # Create your tests here.
+class BadgeTestCase(TestCase):
+    def test_overloaded_model(self):
+        from brabeion.models import BadgeAward
+        user=User(username='me', password='you')
+        user.save()
+        slug='horse'
+        awarded=7
+        extra_kwargs={}
+        BadgeAward.objects.create(user=user, slug=slug, level=awarded, **extra_kwargs)
