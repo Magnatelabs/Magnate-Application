5a15aac4c2909b60b586dd21d545289409b18b0f
jmwilliams(air) <jolumwilliams@gmail.com>
2013-12-09 18:18:47 -0500

    Excluded brabeion from tests

--- app_magnate/app_magnate/settings.py
+++ app_magnate/app_magnate/settings.py
@@ -187,7 +187,7 @@ INSTALLED_APPS = [
 ]
 
 TEST_RUNNER = 'django_test_exclude.runners.ExcludeTestSuiteRunner'
-TEST_EXCLUDE=[ "django.contrib", "zinnia", "account", "waitinglist" ]
+TEST_EXCLUDE=[ "django.contrib", "zinnia", "account", "waitinglist", "brabeion" ]
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
