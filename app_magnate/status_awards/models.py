87236f9530a5ff6658aff5fb686416b0e4b6b987
jmwilliams(air) <jolumwilliams@gmail.com>
2013-12-09 18:46:44 -0500

    Overloaded create_entry_content to describe the new badge

--- app_magnate/status_awards/models.py
+++ app_magnate/status_awards/models.py
@@ -2,5 +2,9 @@ from brabeion.models import BadgeAwardAbstractClass
 from dashboard.mixins import PrivatelyPublishedModelMixin
 
 class MagnateBadgeAward(PrivatelyPublishedModelMixin, BadgeAwardAbstractClass):
+
+    def create_entry_content(self):
+        return "Fantastic! You have just been awarded a Badge %s! ... <p> %s" % (self.name, self.description)
+
     class Meta:
         abstract = True
