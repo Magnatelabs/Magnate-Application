f8cd6c991535900cb83045cfcd349db479bccd4a
jmwilliams(air) <jolumwilliams@gmail.com>
2013-12-11 15:32:46 -0500

    added as_html to display badges as pictures. Also overloaded create_entry_slug

--- app_magnate/status_awards/models.py
+++ app_magnate/status_awards/models.py
@@ -1,10 +1,18 @@
 from brabeion.models import BadgeAwardAbstractClass
 from dashboard.mixins import PrivatelyPublishedModelMixin
+from django.conf import settings
+from django.core.urlresolvers import reverse
 
 class MagnateBadgeAward(PrivatelyPublishedModelMixin, BadgeAwardAbstractClass):
 
     def create_entry_content(self):
         return "Fantastic! You have just been awarded a Badge %s! ... <p> %s" % (self.name, self.description)
+
+    def create_entry_slug(self):
+        return 'award'
+
+    def as_html(self):
+        return "<a href=\"%s\"><img src=\"%s\" alt=\"%s\" ></a>" % (reverse('status_award_detail')+'?award=%d' % (self.pk), settings.STATIC_URL + "img/img_icon13.png", self.name)
 
     class Meta:
         abstract = True
