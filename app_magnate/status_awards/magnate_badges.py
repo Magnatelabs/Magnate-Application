a2369587b401135b3f88b4b00fd0da86530e643b
jmwilliams(air) <jolumwilliams@gmail.com>
2013-12-11 16:26:16 -0500

    Added DonorBadge awarded for donations

--- app_magnate/status_awards/magnate_badges.py
+++ app_magnate/status_awards/magnate_badges.py
@@ -1,6 +1,8 @@
 from brabeion.base import Badge, BadgeAwarded, BadgeDetail
 from social.models import total_likes_by_user
+from donations.utils import all_donations_by_user
 from brabeion import badges
+
 
 class LikesBadge(Badge):
     slug="likes"
@@ -26,5 +28,33 @@ class LikesBadge(Badge):
             return BadgeAwarded(1)
 
 
-    
+
+class DonorBadge(Badge):
+    slug="donor"
+    levels = [
+        BadgeDetail("New Donor", "Donated a lilttle"),
+        BadgeDetail("Intermediate Donor", "Donated more"),
+        BadgeDetail("Advanced Donor", "Donated a lot "),
+        BadgeDetail("Karmic Donor", "Gave away everything")
+    ]
+    events = [
+        "user_donation",
+    ]
+    multiple = False
+
+    def award(self, **state):
+        user = state["user"]
+        donations = all_donations_by_user(user)
+        count = len(donations)
+        amount = sum([d.amount for d in donations])
+        if (amount > 10000):
+            return BadgeAwarded(4)            # Note that conditions on different levels are not monotonous
+        elif (count > 4) and (amount > 500):  # Expected behavior? Do you need lower level to qualify for higher?
+            return BadgeAwarded(3)            # Probably not... But once you qualify for higher, you 
+        elif (count > 2) and (amount > 100):  # won't get the lower ones ;-)
+            return BadgeAwarded(2)
+        elif (count > 0) and (amount > 0):
+            return BadgeAwarded(1)
+
 badges.register(LikesBadge)
+badges.register(DonorBadge)
