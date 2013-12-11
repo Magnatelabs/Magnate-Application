fa7122a3b5addf366d7724a797b7e98fe870096b
jmwilliams(air) <jolumwilliams@gmail.com>
2013-12-11 17:33:21 -0500

    Removed as_html. Moved this functionality to temaplatetags, {% render_badge... %}

--- app_magnate/status_awards/models.py
+++ app_magnate/status_awards/models.py
@@ -1,7 +1,5 @@
 from brabeion.models import BadgeAwardAbstractClass
 from dashboard.mixins import PrivatelyPublishedModelMixin
-from django.conf import settings
-from django.core.urlresolvers import reverse
 
 class MagnateBadgeAward(PrivatelyPublishedModelMixin, BadgeAwardAbstractClass):
 
@@ -10,10 +8,6 @@ class MagnateBadgeAward(PrivatelyPublishedModelMixin, BadgeAwardAbstractClass):
 
     def create_entry_slug(self):
         return 'award'
-
-    def as_html(self):
-        image_url = "%sstatus_awards/badge_%s_%d.png" % (settings.STATIC_URL, self.slug, self.level)
-        return "<a href=\"%s\"><img src=\"%s\" alt=\"%s\" ></a>" % (reverse('status_award_detail')+'?award=%d' % (self.pk), image_url, self.name)
 
     class Meta:
         abstract = True
