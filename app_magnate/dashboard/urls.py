b829ac83b7f238f63b1334d4539b61980706d3d5
jmwilliams(air) <jolumwilliams@gmail.com>
2013-12-12 01:55:31 -0500

    Added updates/ that returns json updates when you get a new badge, there is a new article, etc., and user_badges, to refresh the contents of the div user_badges when there are updates

--- app_magnate/dashboard/urls.py
+++ app_magnate/dashboard/urls.py
@@ -13,5 +13,8 @@ urlpatterns = patterns("",
   url(r"^dashboard/$", DashboardView.as_view(), name="dashboard"),
   url(r"^dashboard/spotlightrequest/$", dashboard_spotlight_request.as_view(), name="spotlight_request"),
 url(r"^spotlight_confirm/$", "dashboard.views.dash_confirm_index", name="confirm_dashboard"),
+
+url(r"updates/$", "dashboard.views.receive_updates_api", name="receive_updates_api"),
+url(r"^user_badges/$", "dashboard.views.user_badges", name="user_badges")
   #  url(r"^dashboard/spotlightrequest/$", "dashboard.views.dashboard_spotlight_request", name="spotlight_request"),
 )
