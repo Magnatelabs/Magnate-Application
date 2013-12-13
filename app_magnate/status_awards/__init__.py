46f894aff7b99c911c7cc453a8b680b3a81b99a2
jmwilliams(air) <jolumwilliams@gmail.com>
2013-12-13 17:36:58 -0500

    Every time a badge is awarded (on signal), try to award appropriate metabadges, specifically depending on which badge has been awarded

--- app_magnate/status_awards/__init__.py
+++ app_magnate/status_awards/__init__.py
@@ -1,3 +1,6 @@
+from brabeion.signals import badge_awarded
+from django.dispatch import receiver
+
 from django.conf import settings
 if not settings.TESTING:
     # importing will register the badges
@@ -11,5 +14,12 @@ else:
 def award_badges(event, user):
     from brabeion import badges
     badges.possibly_award_badge(event, user=user)    
-    
 
+
+
+
+# Every time a badge is awarded, may need to award metabadges
+@receiver(badge_awarded)
+def on_badge_awarded(sender, **kwargs):
+    print 'badge_awarded_' + sender.slug
+    award_badges('badge_awarded_' + sender.slug, user=kwargs['badge_award'].user)
