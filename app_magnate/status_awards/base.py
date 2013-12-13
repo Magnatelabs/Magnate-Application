f42afea040377c5ee9f54db444b65ef58f464d50
jmwilliams(air) <jolumwilliams@gmail.com>
2013-12-13 17:37:57 -0500

    Removed events; now deriving classes have to define. Also the awarding function now properly takes into account the levels that are required for each slug

--- app_magnate/status_awards/base.py
+++ app_magnate/status_awards/base.py
@@ -4,19 +4,20 @@ from django.db.models import Max
 
 # a badge given for collecting other badges
 class MetaBadge(Badge):
-    events = [
-        "badge_awarded"
-    ]
+    #    events =
+    #    When deriving, set events = ['badge_awarded_'+slug for slug in set(slug for s_l in requirements for slug in s_l.keys())]
+
+
     multiple = False
 
     def award(self, **state):
         user=state["user"]
-        
+
         # Returns the highest level achieved by this user for each badge slug, as a dictionary {slug : max_level} 
         achieved = {x['slug'] : x['max_level'] for x in BadgeAward.objects.filter(user=user).values('slug').annotate(max_level=Max('level'))}
 
         for i, r in enumerate(self.requirements):
-            if not all([achieved[slug] >= r[slug] for slug in r]):
+            if not all([slug in achieved and achieved[slug] >= r[slug] for slug in r]):
                 if i > 0:
                     return BadgeAwarded(i)
                 else:
