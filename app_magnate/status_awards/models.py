12d6040ebf14690b2f9732b13ec6f846e35819cf
jmwilliams(air) <jolumwilliams@gmail.com>
2013-12-13 15:56:19 -0500

    Added is_metabadge(self). Checks if this is a regular badge or a meta-badge, given for earning other badges

--- app_magnate/status_awards/models.py
+++ app_magnate/status_awards/models.py
@@ -1,6 +1,8 @@
 from brabeion.models import BadgeAwardAbstractClass
 from dashboard.mixins import PrivatelyPublishedModelMixin
 
+
+
 class MagnateBadgeAward(PrivatelyPublishedModelMixin, BadgeAwardAbstractClass):
 
     def create_entry_content(self):
@@ -9,5 +11,11 @@ class MagnateBadgeAward(PrivatelyPublishedModelMixin, BadgeAwardAbstractClass):
     def create_entry_slug(self):
         return 'badge'
 
+    # Metabadges are special badges that are awarded for receiving other badges
+    def is_metabadge(self):
+        from .base import MetaBadge
+        return isinstance(self._badge, MetaBadge)
+
     class Meta:
         abstract = True
+
