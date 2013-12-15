48706b4357d4e5761e27ed3f3036772ae5521f76
jmwilliams(air) <jolumwilliams@gmail.com>
2013-12-14 20:46:38 -0500

    Now validating badges on startup. Checking that they have levels, events; if there are requirements (metabadge), then there are as many of them as there are levels. If we are not in TESTING mode, checking if all the files with badge pictures can be found

--- app_magnate/app_magnate/startup.py
+++ app_magnate/app_magnate/startup.py
@@ -16,5 +16,38 @@ def autoload(submodules):
                     raise
 
 
+def startup_validation():
+    from brabeion import badges
+    from status_awards.base import MetaBadge
+    # check if static resources exist
+    from django.contrib.staticfiles import finders
+    from django.conf import settings
+
+    slug_set = set()
+    for badge_slug in badges._registry:
+        assert badge_slug not in slug_set
+        slug_set.add(badge_slug)
+
+        badge = badges._registry[badge_slug]
+        assert badge.slug == badge.slug
+        assert hasattr(badge, 'levels')
+        assert hasattr(badge, 'events')
+        assert isinstance(badge.levels, list)
+        assert isinstance(badge.events, list)
+        assert len(badge.levels) > 0
+        for e in badge.events:
+            assert isinstance(e, str)
+            assert e in badges._event_registry
+        if isinstance(badge, MetaBadge):
+            assert hasattr(badge, 'requirements')
+            assert len(badge.requirements) == len(badge.levels)
+        if not settings.TESTING:
+            for level in range(len(badge.levels)):
+                # Make sure that the picture for the badge exists
+                pic_url = 'status_awards/badge_%s_%d.png' % (badge_slug, level)
+                assert finders.find(pic_url) is not None, "Cannot find necessary static file %s" % pic_url
+
+
 def run():
     autoload(["receivers"])
+    startup_validation()
