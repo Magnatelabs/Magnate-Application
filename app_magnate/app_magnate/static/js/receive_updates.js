0448de67096db739ebf4c856a96d2facc7c283dc
jmwilliams(air) <jolumwilliams@gmail.com>
2013-12-16 03:13:10 -0500

    Now receiving updates right away on startup so we can better detect changes.

--- app_magnate/app_magnate/static/js/receive_updates.js
+++ app_magnate/app_magnate/static/js/receive_updates.js
@@ -6,13 +6,15 @@ if (window.refreshIntervalId != undefined) {
 window.refreshPeriodMs = 2000;
 var last_ts = 0;
 
-window.refreshIntervalId = setInterval( function() {
+function receive_updates() {
+   
 
     $.getJSON("/dash/updates/?strictlyafter=" + last_ts, function(data) {
-
+	console.log("received json " + data.badges.length + " "  + last_ts);
 	if (last_ts != 0) { // Do nothing the first time. 
 	    if (data.badges.length > 0) {
 	        for (var i=0; i<data.badges.length; i++) {
+                    console.log("unlocked badge " + data.badges[i].name);
 	            alert("You've unlocked a new badge: "+data.badges[i].name);
 	        }
 	        $("#user_badges").load("/dash/user_badges/");
@@ -20,4 +22,11 @@ window.refreshIntervalId = setInterval( function() {
 	}
 	last_ts = data.last_ts
     });
-}, window.refreshPeriodMs );
+}
+
+// Call right away to initialize. So that if something happens, we can detect that this is new. 
+// (Better solutions are possible, but this is still better than nothing. Otherwise if the user e.g. likes something within the first second, before the first timer event, he/she won't see the badges for that, if any, until page refresh.)
+receive_updates();
+
+
+window.refreshIntervalId = setInterval(receive_updates, window.refreshPeriodMs );
