d294b6f3d3758803832417169cf4c9f1e065dd2d
jmwilliams(air) <jolumwilliams@gmail.com>
2013-12-16 00:48:58 -0500

    Added view ajax_star_rating

--- app_magnate/social/urls.py
+++ app_magnate/social/urls.py
@@ -6,5 +6,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns("",
-url(r"^social/like/$", "social.views.ajax_like_entry", name='ajax_like_entry'),                       
+url(r"^social/like/$", "social.views.ajax_like_entry", name='ajax_like_entry'), 
+url(r"^social/rate/$", "social.views.ajax_star_rating", name='ajax_star_rating'),     
 )
