82e4ece2ea64e15213bebb2a6521fb886c37d85a
jmwilliams(air) <jolumwilliams@gmail.com>
2013-12-16 20:16:53 -0500

    1. In ajax_like_entry now using a decorator @require_POST instead of an if. 2. In ajax_star_rating now if the user is not authenticated, not trying to get the value of a rating. Also if the value is None, just returning, not trying to cast it to int

--- app_magnate/social/views.py
+++ app_magnate/social/views.py
@@ -7,7 +7,6 @@ from .templatetags import social_tags
 from django.http import HttpResponseBadRequest
 from django.utils import simplejson
 from django.views.decorators.http import require_POST
-from django.contrib.auth.decorators import login_required
 
 from .models import toggle_like_unlike, total_entry_likes, StarRating, can_rate
 import status_awards
@@ -15,6 +14,7 @@ import status_awards
 #TODO! FIXME! Think about thread safety if multiple users like the same post at the same time!
 
 # User clicked the "Like" button after a post.
+@require_POST
 def ajax_like_entry(request):
     if not request.is_ajax():
         return HttpResponseBadRequest()
@@ -22,30 +22,31 @@ def ajax_like_entry(request):
         return HttpResponse(status=401)
 
     vars={}
-    if request.method=='POST':
-        user=request.user
-        entry_id=request.POST.get('entry_id', None)
-        entry=get_object_or_404(Entry, pk=entry_id)
 
-        count = toggle_like_unlike(entry, user)
-        status_awards.award_badges("user_liked_entry", user)
+    user=request.user
+    entry_id=request.POST.get('entry_id', None)
+    entry=get_object_or_404(Entry, pk=entry_id)
+
+    count = toggle_like_unlike(entry, user)
+    status_awards.award_badges("user_liked_entry", user)
 
 
-        vars['liked'] = (count > 0)
-        vars['total_likes'] = total_entry_likes(entry)
-        vars['update_html']={social_tags.get_div_dom_id(entry_id) : social_tags.like_entry_button(int(entry_id), user)}
-        return HttpResponse(simplejson.dumps(vars), mimetype='application/javascript')
-    else:
-        return HttpResponseBadRequest()
+    vars['liked'] = (count > 0)
+    vars['total_likes'] = total_entry_likes(entry)
+    vars['update_html']={social_tags.get_div_dom_id(entry_id) : social_tags.like_entry_button(int(entry_id), user)}
+    return HttpResponse(simplejson.dumps(vars), mimetype='application/javascript')
 
 @require_POST
-@login_required
 def ajax_star_rating(request):
     if not request.is_ajax():
         return HttpResponseBadRequest()
-    value = int(request.POST.get('value', None))
+    if not request.user.is_authenticated():
+        return HttpResponse(status=401)
+
+    value = request.POST.get('value', None)
     if value is None:
         return HttpResponseBadRequest()
+    value = int(value)
 
     var={}
     if can_rate(request.user):
