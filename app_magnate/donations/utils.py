dfff2ecbc7e2eaee68e3421db1020787d1729b27
jmwilliams(air) <jolumwilliams@gmail.com>
2014-04-11 12:03:21 -0700

    added a datetime utility to the donations application

--- app_magnate/donations/utils.py
+++ app_magnate/donations/utils.py
@@ -1,5 +1,6 @@
 from .models import Donation
 from django.conf import settings
+import datetime
 
 # username is a string, such as 'admin'
 # Returns a Decimal
@@ -9,3 +10,5 @@ def total_donation_amount(username):
 def all_donations_by_user(username):
     return Donation.objects.filter(user__username=username)
 
+def donation_vesting_date(username):
+    return datetime.datetime(2015,1,1)
