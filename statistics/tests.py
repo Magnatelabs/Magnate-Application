a45baccbac9a038c57701866145144d9dc2873cf
Sergey Orshanskiy <orshansk@gmail.com>
2014-05-26 16:16:11 -0400

    Now testing simple get_stat and set_stat, including get_stat for a nonexistent statistic

--- statistics/tests.py
+++ statistics/tests.py
@@ -6,11 +6,13 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-
+from .models import get_stat, set_stat
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
         """
         Tests that 1 + 1 always equals 2.
         """
-        self.assertEqual(1 + 1, 2)
+        self.assertEqual(get_stat('NONEXISTENT_STATISTIC'), None)
+        set_stat('APPLE', -1.738564)
+        self.assertEqual(get_stat('APPLE'), -1.738564, 0.000001)
