4f16e5c8fc7947d3a00ae9321f658ec22e169e49
Sergey Orshanskiy <orshansk@gmail.com>
2014-05-26 01:13:29 -0400

    Added Cling() to serve static files directly from django -- for now

--- app_magnate/app_magnate/wsgi.py
+++ app_magnate/app_magnate/wsgi.py
@@ -24,7 +24,8 @@ startup.run()
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+application = Cling(get_wsgi_application())
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
