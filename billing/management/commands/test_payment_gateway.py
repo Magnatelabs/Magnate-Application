febe68ebc4d05ba6de5107703fca247a403eee93
Sergey Orshanskiy <orshansk@gmail.com>
2014-05-27 01:35:11 -0400

    1.x_amount for the test transaction is now the current time mod 86400, so it is quite unique, to avoid duplicate transactions; 2. x_relay_url now points to Heroku TO THE RUNNING INSTANCE (hard-coded url). So we are trying to put some bogus transaction into an actual instance. THE URL IS HARD-CODED FOR NOW. 3. Now checking for errors, if the result is not a code snippet (communication of the gateway with the website failed) vs. if it is a snippet, detecting if it is on success or on failure. If anything went wrong, raising an error. 4. More handy output

--- billing/management/commands/test_payment_gateway.py
+++ billing/management/commands/test_payment_gateway.py
@@ -12,12 +12,13 @@ import urllib2
 entry = {
     'first_name': 'Martha',
     'last_name': 'Graham',
-    'zip': 98015,
+    'zip': 98017,
     'address': '1300 East 62nd st. 2nd floor',
     'city': 'New York',
     'country': 'USA',
     'card_num': '4007000000027', # Visa test card for Authorize.net
     'exp_date': '01/28', # expires in 2028
+    'relay_url': 'http://magnate-prod.herokuapp.com/donations/authorize_net/authorize_net-notify-handler/',
 }
 
 class Command(NoArgsCommand):
@@ -28,12 +29,12 @@ class Command(NoArgsCommand):
         print "So should show an error, but mention that the transaction has been approved"
 
         int_obj = get_integration("authorize_net_dpm")
-        fields = {'x_amount': 1.23,
+        fields = {'x_amount': time.time() % 86400 , # fairly unique amount so we do not worry about duplicate transactions 
                   'x_fp_sequence': datetime.datetime.now().strftime('%Y%m%d%H%M%S'), # any identifier for the transaction
                   'x_fp_timestamp': str(int(time.time())), # the timestamp when x_fp_sequence was generated
                   'x_recurring_bill': 'F',
-                  'x_relay_url': "http://some.url/authorize_net_notify_handler",
-                  'x_cust_id': 'test''',
+                  'x_relay_url': entry['relay_url'],
+                  'x_cust_id': 'test',
                   'x_first_name': entry['first_name'],
                   'x_last_name': entry['last_name'],
                   'x_zip': entry['zip'],
@@ -53,8 +54,27 @@ class Command(NoArgsCommand):
         query_dict = {k:query_dict[k] for k in query_dict if query_dict[k] is not None}
         query_str = urllib.urlencode(query_dict)
         print 'Issuing POST request...'
+        print query_str
         result = urllib2.urlopen(int_obj.service_url, query_str)
 
+        print "\nYou should see a redirect snippet on success:\n"
         # need result.getcode()==200
-        print result.readlines()
-              
+        lines=result.readlines()
+        print lines, "\n"
+
+        is_snippet=False
+        transaction_approved=False
+        for l in lines:
+            if 'script' in l.lower(): # Javascript expected to redirect the user's browser on success or failure, indicating that Authorize.Net was able to receive a response from our website. So it successfully reported this (sample, fake) transaction to us.
+                is_snippet=True
+            if 'approved' in l.lower():
+                transaction_approved = True
+                
+
+        if not is_snippet:
+            raise CommandError('Payment gateway failed to communicate the transaction to the website. API flow failure. The gateway should return a snippet to redirect the user to a success/failure page')
+
+        if not transaction_approved:
+            raise CommandError('Transaction failed, even though the payment gateway successfully communicated this fact to the website.')
+
+        print "\nOK, Success"
