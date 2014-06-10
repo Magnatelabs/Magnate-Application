1446b48e08e557cbf438a8a9fa5a4a2f5f6f4f83
Sergey Orshanskiy <orshansk@gmail.com>
2014-06-10 03:03:57 -0400

    Added RewardsView(EntryIndex). It uses rewards_home.html as the template and redefines get_queryset to publish only public entries that have an associated Hangout

--- rewards/views.py
+++ rewards/views.py
@@ -9,8 +9,17 @@ import datetime
 from companyinfo.models import MediaUpdates
 from companyinfo.forms import MediaUpdatesForm
 
+from zinnia.views.archives import EntryIndex
+from zinnia.models.entry import Entry
+
+class RewardsView(EntryIndex):
+    def get_template_names(self):
+        return ['rewards/rewards_home.html']
+    def get_queryset(self): # TODO: unit test that we only show published entries, and only those that have an associated hangout
+        return Entry.published.filter(hangout__isnull=False)
 
 def rewards_index(request):
-    return render(request, 'rewards/rewards_home.html')
+    return RewardsView.as_view()(request, template_name='rewards/rewards_home.html')
+#    return render(request, 'rewards/rewards_home.html')
 
 
