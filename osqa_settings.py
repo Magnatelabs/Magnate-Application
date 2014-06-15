e705bd3c3ddfbab555328273ad5d85bd6e0ebd16
Sergey Orshanskiy <orshansk@gmail.com>
2014-06-14 23:16:46 -0400

    minimum settings for OSQA, so at least it can properly live side-by-side with Magnate, even if not functioning for now

---
+++ osqa_settings.py
@@ -0,0 +1,13 @@
+# This should be equal to your domain name, plus the web application context.
+# This shouldn't be followed by a trailing slash.
+# I.e., http://www.yoursite.com or http://www.hostedsite.com/yourhostapp
+APP_URL = 'http://'
+
+
+app_url_split = APP_URL.split("://")
+
+APP_PROTOCOL = app_url_split[0]
+APP_DOMAIN = app_url_split[1].split('/')[0]
+APP_BASE_URL = '%s://%s' % (APP_PROTOCOL, APP_DOMAIN)
+
+OSQA_DEFAULT_SKIN = 'default'
