1f93eaa941c45e5468d6d21f6ebefd8ad466fd71
Sergey Orshanskiy <orshansk@gmail.com>
2014-06-15 20:47:27 -0400

    Fix: now intelligently inserting the modified Privacy section at the place where at was, not just at position 2 (which changes when Zinnia changes)

--- glue_zinnia/admin.py
+++ glue_zinnia/admin.py
@@ -10,13 +10,25 @@ class EntryCheckAdmin(EntryAdmin):
 #  print '!!!'
 #  print EntryAdmin.fieldsets[2]
 #  print EntryAdmin.filter_horizontal
-  fieldsets = EntryAdmin.fieldsets[:2] + ((_('Privacy'), {'fields': (
+
+
+  # Detect if there is a section called Privacy. There probably is.
+  # Remove it, if it is present, and insert a new Privacy section at the end.
+  l = EntryAdmin.fieldsets
+  fieldsets = [x for x in l if unicode(x[0]) != 'Privacy'] + [(_('Privacy'), {'fields': (
       'login_required', 'authorized_users', 'password'), 
-  'classes': ('collapse', 'collapse-closed')}),) + \
-    EntryAdmin.fieldsets[3:]
+  'classes': ('collapse', 'collapse-closed')}),] 
+
+# This code below stopped working after Zinnia added another section to admin.
+#
+#  fieldsets = EntryAdmin.fieldsets[:2] + ((_('Privacy'), {'fields': (
+#      'login_required', 'authorized_users', 'password'), 
+#  'classes': ('collapse', 'collapse-closed')}),) + \
+#    EntryAdmin.fieldsets[3:]
   filter_horizontal = EntryAdmin.filter_horizontal + ('authorized_users',)
 
 # Unregister the default EntryAdmin
 # then register the EntryCheckAdmin class
+
 admin.site.unregister(Entry)
 admin.site.register(Entry, EntryCheckAdmin)
