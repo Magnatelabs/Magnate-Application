34dcff0cdc5776c1bf2d91ebaf1e84fba61ade5a
Sergey Orshanskiy <orshansk@gmail.com>
2014-06-15 23:25:20 -0400

    Code linking OSQA with our Magnate app

---
+++ glue_osqa/tests.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+
+# Create your tests here.
+class FirstOSQATest(TestCase):
+    def testSomething(self):
+        from django.conf import settings
+        # make sure this setting is defined
+        # otherwise it fails in subtle ways.
+        # For example, you can get IntegrityError: duplicate key violates unique constraint
+        # in prop.save() in forum/models/user.py in __setattr__
+        # Because it will use UserProperty.objects.get to see if this property already exists, 
+        # but in BaseModel in forum/models/base.py, 
+        # objects = CachedManager(). So if some cache configuration is
+        # broken... you get the idea. 
+        self.assertTrue(hasattr(settings, 'CACHE_MAX_KEY_LENGTH'))
+
