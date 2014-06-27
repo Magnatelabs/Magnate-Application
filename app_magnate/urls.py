a1554c1519b3123eded591c3f6d7e68f61540830
jmwilliams(air) <jolumwilliams@gmail.com>
2014-06-26 18:42:07 -0700

    uncommented out a part of the patterns function which was effecting Zinnia content from displaying

--- app_magnate/urls.py
+++ app_magnate/urls.py
@@ -63,7 +63,7 @@ urlpatterns = patterns("",
 
 urlpatterns += patterns('',
                         (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-#    (r'^site_media/media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    (r'^site_media/media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 )
 
 
