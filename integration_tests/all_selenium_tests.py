ada05e07fd740aa2bc705321404350af163fea5f
Sergey Orshanskiy <orshansk@gmail.com>
2014-07-09 20:49:09 -0400

    Switched from Selenium 1 to Selenium 2. Now using webdriver.Firefox(). No need to run a separate Selenium server

--- integration_tests/all_selenium_tests.py
+++ integration_tests/all_selenium_tests.py
@@ -13,18 +13,31 @@ import sys
 print "All Selenium Tests for Magnate"
 print "Expecting Django server on port 8123"
 
+import urlparse
+def _(path):
+    return urlparse.urljoin('http://127.0.0.1:8123/', path)
+
 class main_page(unittest.TestCase):
     def setUp(self):
         self.verificationErrors = []
         
-        self.selenium = selenium("localhost", 4444, "*chrome", "http://127.0.0.1:8123/")
-        self.selenium.start()
+###        self.selenium = selenium("localhost", 4444, "*chrome", "http://127.0.0.1:8123/")
+###        self.selenium.start()
+   
         #self.selenium.set_timeout(1000) # do not do it; default 30000=30sec is OK
 
-    def test_main_page(self):
-        sel = self.selenium
-        sel.open("/")
+        self.driver = webdriver.Firefox()
 
+    def test_main_page(self):
+#        sel = self.selenium
+#        sel.open("/wefewwef/")
+#        sel.open("/account/login/")
+
+        # See http://selenium-python.readthedocs.org/en/latest/getting-started.html#using-selenium-to-write-tests
+        driver = self.driver
+        driver.get(_("/"))
+        self.assertIn('Unleash your inner mogul', driver.page_source)
+        
 #        sel.click("nav_tags")
 #        sel.wait_for_page_to_load("30000")
 #        try: self.failUnless(sel.is_text_present("Tag list"))
@@ -35,8 +48,9 @@ class main_page(unittest.TestCase):
 #        except AssertionError, e: self.verificationErrors.append(str(e))
     
     def tearDown(self):
-        self.selenium.stop()
-        self.assertEqual([], self.verificationErrors)
+  ###      self.selenium.stop()
+  ###      self.assertEqual([], self.verificationErrors)
+        self.driver.close()
 
 if __name__ == "__main__":
     unittest.main()
