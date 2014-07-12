fe49d5bff7474f8d5ac6322e29117bf26d3a9363
Sergey Orshanskiy <orshansk@gmail.com>
2014-07-11 23:53:39 -0400

    Changed Loglevel to INFO

--- app_magnate/settings.py
+++ app_magnate/settings.py
@@ -275,7 +275,7 @@ LOGGING = {
     },
     'handlers': {
         'console': {
-            'level': 'DEBUG',
+            'level': 'INFO',
             'class': 'logging.StreamHandler',
             'formatter': 'heroku-style'
         },
