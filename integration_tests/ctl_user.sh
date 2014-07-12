cf340a409492b20101c74013e1b190e4cb8abd93
Sergey Orshanskiy <orshansk@gmail.com>
2014-07-12 18:16:02 -0400

    Refactored to user glue_osqa.user.has_user and glue_osqa.user.create_user

--- integration_tests/ctl_user.sh
+++ integration_tests/ctl_user.sh
@@ -18,21 +18,8 @@ source "$DIR/test_credentials.pysh"
 function has_user() {
   USERNAME="$1"
   PASSWORD="$2"
-	output=`echo -e "from django.test import Client
-c = Client()
-if c.login(username=\"$USERNAME\", password=\"$PASSWORD\"):
-  print 0; exit()
-
-import forum
-from forum.models.user import User
-try:
-  User.objects.get(username='$USERNAME')
-  print 2; exit()
-except forum.models.user.User.DoesNotExist:
-  print 1; exit()
-
-print 3; exit()" | heroku run python manage.py shell --app $HEROKU_APP | tail -n 1`
-  return $output
+	echo -e "from glue_osqa.user import has_user
+print has_user(\"$USERNAME\", \"$PASSWORD\"); exit()" | heroku run python manage.py shell --app $HEROKU_APP | tail -n 1
 }
 
 # Create a user in a django Heroku app. Uses user model forum.models.User
@@ -44,30 +31,8 @@ print 3; exit()" | heroku run python manage.py shell --app $HEROKU_APP | tail -n
 function create_user() {
   USERNAME="$1"
   PASSWORD="$2"
-	output=`echo -e "import forum
-from forum.models.user import User
-try:
-  User.objects.get(username='$USERNAME')
-  print 1; exit()
-except forum.models.user.User.DoesNotExist:
-  try:
-    u=User.objects.create(username='$USERNAME')
-    u.set_password('$PASSWORD')
-    u.save()
-    print 0; exit()
-  except Exception as e:
-    print str(e); exit()
-
-exit()" | heroku run python manage.py shell --app $HEROKU_APP`
-  last_output_line=`echo "$output" | tail -n 1`
-  [ "$last_output_line" == "0" ] && return 0
-  if [ "$last_output_line" == "1" ]; 
-  then
-    error="User '$USERNAME' already exists"
-    return 1
-  fi
-	error="$output"
-  return 2
+	echo -e "from glue_osqa.user import create_user
+print str(create_user(\"$USERNAME\", \"$PASSWORD\")); exit()" | heroku run python manage.py shell --app $HEROKU_APP | tail -n 1
 }
 
 
@@ -110,34 +75,4 @@ esac
 
 echo "Cmd: $cmd, user: $user"
 echo "Connecting to heroku app $HEROKU_APP..."
-if [ "$cmd" == "has_user" ]; 
-then
-  has_user "$user" "$passwd"
-  case $? in
-    0)
-      echo "There IS a user '$user'"
-      ;;
-    1)
-      echo "There is NO user '$user'"
-      ;;
-    2)
-      echo "Wrong password, but the user '$user' exists"
-      ;;
-    *)
-      echo "Unknown error"
-      ;;
-  esac
-elif [ "$cmd" == "create_user" ];
-then
-  create_user "$user" "$passwd"
-  code=$?
-  if [ $code -eq 0 ]; then
-    echo "OK, created user '$user'"
-  else
-    echo "ERROR: cannot create the user '$user'"
-    echo "$error"
-  fi
-else
-  usage
-  exit
-fi
+$cmd "$user" "$password"
