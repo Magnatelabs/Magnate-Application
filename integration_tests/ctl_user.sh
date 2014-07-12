5de864e0969f8bb6762abf728004e73998ac556b
Sergey Orshanskiy <orshansk@gmail.com>
2014-07-12 18:26:35 -0400

    Refactoring

--- integration_tests/ctl_user.sh
+++ integration_tests/ctl_user.sh
@@ -6,33 +6,14 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # The file is in the same directory as the script itself
 source "$DIR/test_credentials.pysh"
 
-# Check if there is such a user in a django Heroku app.
-# Log into Heroku using heroku run python manage.py shell 
-# Assumes that the name of the heroku app is $HEROKU_APP
-# @arg  USERNAME
-# @arg   PASSWORD
-# @returns   0 if there is such a user (USERNAME, PASSWORD), 
-#            1 if there is no such user USERNAME,
-#            2 if the password is incorrect, but the user exists,
-#            3 otherwise (just in case)
-function has_user() {
-  USERNAME="$1"
-  PASSWORD="$2"
-	echo -e "from glue_osqa.user import has_user
-print has_user(\"$USERNAME\", \"$PASSWORD\"); exit()" | heroku run python manage.py shell --app $HEROKU_APP | tail -n 1
-}
 
-# Create a user in a django Heroku app. Uses user model forum.models.User
-# Assumes that the name of the heroku app is $HEROKU_APP
-# @arg  USERNAME
-# @arg  PASSWORD
-# @returns   0 on success, 1 if the user already exists, 2 on other errors
-# Sets $error if the return value is not 0.
-function create_user() {
-  USERNAME="$1"
-  PASSWORD="$2"
-	echo -e "from glue_osqa.user import create_user
-print str(create_user(\"$USERNAME\", \"$PASSWORD\")); exit()" | heroku run python manage.py shell --app $HEROKU_APP | tail -n 1
+
+function remote_command() {
+  CMD="$1"
+  USERNAME="$2"
+  PASSWORD="$3"
+	echo -e "from glue_osqa.user import $CMD
+print $CMD(\"$USERNAME\", \"$PASSWORD\"); exit()" | heroku run python manage.py shell --app $HEROKU_APP | tail -n 1
 }
 
 
@@ -75,4 +56,5 @@ esac
 
 echo "Cmd: $cmd, user: $user"
 echo "Connecting to heroku app $HEROKU_APP..."
-$cmd "$user" "$password"
+
+remote_command "$cmd" "$user" "$password"
