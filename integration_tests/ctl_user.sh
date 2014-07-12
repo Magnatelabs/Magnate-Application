0748c6a86666c07edcbb5cb21f652ba38264ac28
Sergey Orshanskiy <orshansk@gmail.com>
2014-07-12 19:45:30 -0400

    Now can pass 'local' as the first argument to connect to the local installation, not remote on heroku

--- integration_tests/ctl_user.sh
+++ integration_tests/ctl_user.sh
@@ -2,7 +2,7 @@
 
 HEROKU_APP=magnate-prod
 REMOTE_SHELL="heroku run python manage.py shell --app $HEROKU_APP"
-# REMOTE_SHELL="python manage.py shell"
+LOCAL_SHELL="python manage.py shell"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # The file is in the same directory as the script itself
@@ -22,13 +22,20 @@ print $CMD(\"$USERNAME\", \"$PASSWORD\"); exit()" | $REMOTE_SHELL | tail -n 1
 function usage {
   name=`basename $0`
   echo "This tool connects to Heroku with 'heroku run python manage.py shell'"
-  echo "Usage:"
+  echo "Usage: $name [local] <command> [username] [password]"
+  echo
   echo "$name has-test-user       See if there is a test user '$TEST_USERNAME'"
   echo "$name create-test-user    Create a test user '$TEST_USERNAME'"
   echo "$name has-user <user> <passwd>"
   echo "$name create-user <user> <passwd>"
   echo "$name create-siteowner <user> <passwd>"
 }
+
+if [ "$1" == "local" ]; 
+then
+  REMOTE_SHELL=$LOCAL_SHELL
+  shift
+fi
 
 case $1 in
   has-test-user)
@@ -63,6 +70,6 @@ case $1 in
 esac
 
 echo "Cmd: $cmd, user: $user"
-echo "Connecting to heroku app $HEROKU_APP..."
+echo "Connecting to $REMOTE_SHELL..."
 
 remote_command "$cmd" "$user" "$passwd"
