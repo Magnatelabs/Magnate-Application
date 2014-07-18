db1b42521510e8ebab77b34309d8b43289d7b734
jmwilliams(air) <jolumwilliams@gmail.com>
2014-07-18 09:39:00 -0700

    modified file in order to include the URL for feedback_entry class-based view

--- social/urls.py
+++ social/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 from social import views 
+from social.views import feedback_entry 
 
 from django.contrib import admin
 admin.autodiscover()
@@ -8,5 +9,6 @@ admin.autodiscover()
 urlpatterns = patterns("",
 url(r"^social/like/$", "social.views.ajax_like_entry", name='ajax_like_entry'), 
 url(r"^social/rate/$", "social.views.ajax_star_rating", name='ajax_star_rating'),
-url(r"^users/$", "social.views.user_pages", name='user_page'),     
+url(r"^users/$", "social.views.user_pages", name='user_page'),
+url(r"^feedback/$", feedback_entry.as_view(), name="platform_main"),
 )
