357f644f0d2deda9bc202cf2c882fe914de5314e
jmwilliams(air) <jolumwilliams@gmail.com>
2014-07-19 10:38:24 -0700

    New application folder contains added models, forms, views and urls for the share emails with friends feature

---
+++ share/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.utils import timezone
+from django import forms
+from django.contrib.auth.models import User
+
+
+class EmailShareModel(models.Model):
+    user = models.ForeignKey(User)
+    created = models.DateTimeField(editable=False)
+    share_email_list = models.TextField(max_length=750)
+    share_email_content = models.TextField(max_length=750)
+
+    def __unicode__(self):
+        return self.first_name
---
+++ share/views.py
@@ -0,0 +1,65 @@
+from django.shortcuts import get_object_or_404, render, redirect
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
+from django.utils import timezone
+import datetime 
+
+#imports to recognize django messages
+from django.contrib import messages
+from django.utils.translation import ugettext_lazy as _
+
+#imports to recognize forms 
+from share.forms import EmailShareModelForm
+from share.models import EmailShareModel
+
+#import to recognize class based view
+from django.views.generic.edit import FormView
+
+
+class share_index(FormView):
+#    import pdb #(test for checking each process)
+#    pdb.set_trace()
+     
+    template_name = 'share/_share_lightbox.html'
+    form_class = EmailShareModelForm
+    redirect_field_name = "next"
+    messages = {
+        "survey_added": {
+            "level": messages.SUCCESS,
+            "text": _(u"Your information was successfully submitted.")
+        },
+        "input_error": {
+            "level": messages.ERROR,
+            "text": _(u"Your information was not entered correctly. Please try again.")
+        }
+    }
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST, request.FILES)
+        if form.is_valid():
+            # Process the data in form.cleaned_data
+            entry = EmailShareModel()
+            entry.share_email_list = form.data['description']
+            entry.share_email_content = form.data['analysis']
+            entry.created = datetime.datetime.now()
+            entry.user = request.user
+            entry.save()
+	    messages.add_message(
+                self.request,
+                self.messages["survey_added"]["level"],
+                self.messages["survey_added"]["text"]
+            )
+    
+            return redirect('submit_completed') # Redirect after POST
+        else:
+            messages.add_message(
+                self.request,
+                self.messages["input_error"]["level"],
+                self.messages["input_error"]["text"]
+            )
+            print 'DEBUG: form is not valid!!!', form.errors
