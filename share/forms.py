87c5b3e79a930d8f49660ef29fa7dcf6652f5e57
jmwilliams(air) <jolumwilliams@gmail.com>
2014-07-22 13:48:17 -0700

    Removed incorrect labels on the forms widget, these were preventing the form from functioning properly

--- share/forms.py
+++ share/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import EmailShareModel
+from share.models import EmailShareModel
 from django.utils.safestring import mark_safe
 
 
@@ -8,5 +8,5 @@ class EmailShareModelForm(forms.Form):
     share_email_content = forms.CharField(max_length=750)
 
     def __init__(self, *args, **kwargs):
-        super(StudyModelForm, self).__init__(*args, **kwargs)
-        self.fields['entity'].widget.attrs['id'] = 'super_first'
+        super(EmailShareModelForm, self).__init__(*args, **kwargs)
+        self.fields['share_email_list'].widget.attrs['id'] = 'super_first'
