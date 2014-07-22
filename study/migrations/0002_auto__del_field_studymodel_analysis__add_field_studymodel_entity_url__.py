05a0e7759b19a3f3e38fe3a36cbde03b0012e16b
jmwilliams(air) <jolumwilliams@gmail.com>
2014-07-22 14:18:50 -0700

    Schemamigration for modified study model file

---
+++ study/migrations/0002_auto__del_field_studymodel_analysis__add_field_studymodel_entity_url__.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Deleting field 'StudyModel.analysis'
+        db.delete_column(u'study_studymodel', 'analysis')
+
+        # Adding field 'StudyModel.entity_url'
+        db.add_column(u'study_studymodel', 'entity_url',
+                      self.gf('django.db.models.fields.CharField')(default=0, max_length=255),
+                      keep_default=False)
+
+        # Adding field 'StudyModel.industry'
+        db.add_column(u'study_studymodel', 'industry',
+                      self.gf('django.db.models.fields.CharField')(default=0, max_length=255),
+                      keep_default=False)
+
+
+        # Changing field 'StudyModel.description'
+        db.alter_column(u'study_studymodel', 'description', self.gf('django.db.models.fields.TextField')(max_length=750))
+
+    def backwards(self, orm):
+        # Adding field 'StudyModel.analysis'
+        db.add_column(u'study_studymodel', 'analysis',
+                      self.gf('django.db.models.fields.TextField')(default=0, max_length=750),
+                      keep_default=False)
+
+        # Deleting field 'StudyModel.entity_url'
+        db.delete_column(u'study_studymodel', 'entity_url')
+
+        # Deleting field 'StudyModel.industry'
+        db.delete_column(u'study_studymodel', 'industry')
+
+
+        # Changing field 'StudyModel.description'
+        db.alter_column(u'study_studymodel', 'description', self.gf('django.db.models.fields.CharField')(max_length=255))
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'study.studymodel': {
+            'Meta': {'object_name': 'StudyModel'},
+            'created': ('django.db.models.fields.DateTimeField', [], {}),
+            'description': ('django.db.models.fields.TextField', [], {'max_length': '750'}),
+            'docfile': ('django.db.models.fields.files.FileField', [], {'max_length': '100'}),
+            'entity': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'entity_url': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'industry': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"})
+        }
+    }
+
+    complete_apps = ['study']
