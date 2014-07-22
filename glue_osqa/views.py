28f7d33627d3fb5079b020508f6fd296e82597a8
Sergey Orshanskiy <orshansk@gmail.com>
2014-07-22 14:28:43 -0400

    Removed glue_osqa/urls.py and glue_osqa/views.py. Those are not used; just for testing

--- glue_osqa/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.shortcuts import get_object_or_404, render, redirect, render_to_response
-
-
-def question_page(request):
-    return render(request, 'glue_osqa/question_main.html')
