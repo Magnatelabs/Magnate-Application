8b450414de32228f1068fb7fa049130d06952285
Sergey Orshanskiy <orshansk@gmail.com>
2014-07-24 01:01:40 -0400

    OSQA module for saving settings for various Magnate funds. This way it is handy to store statistics for various Magnate funds, access it programmatically and edit through admin

---
+++ forum_modules/magnate/settings.py
@@ -0,0 +1,70 @@
+from forum.settings.base import Setting, SettingSet
+from django.forms.widgets import Textarea
+from django.core.urlresolvers import reverse
+from forum.settings import APP_URL
+
+
+MAGNATE_FUNDS = SettingSet('MAGNATE_FUNDS', 'Magnate Funds', "Setttings for Magnate Funds", 3000)
+
+MAGNATE_FUND_TEMPLATES={}
+
+from donations.models import MagnateFund
+for fund in MagnateFund.objects.all():
+	FUND_METRICS_TEMPLATE = Setting('FUND_%d_METRICS_TEMPLATE' % (fund.pk),
+	"""        <div class="fund_metrics-container">
+        <div class="metrics-contentinner">
+			<div class="metric">
+			<article>
+			<div class="metric-text">
+			<p>Number of new Investments</p>
+			</div>
+			<div class="metric-amount1">
+			<p>17</p>			
+			</div>
+			</article>       
+			</div>
+			
+			<div class="metric">
+			<article>
+			<div class="metric-amount2">
+			<p>$62,205</p>	
+			</div>
+			<div class="metric-text2">
+			<p>New Asset Inflows</p>
+			</div>
+			</article>       
+			</div>
+			
+			<div class="metric">
+			<article>
+			<div class="metric-amount2">
+			<p>$32,603</p>	
+			</div>
+			<div class="metric-text2">
+			<p>Number of Participants</p>
+			</div>
+			</article>       
+			</div>
+			
+			<div class="metric">
+			<article>
+			<div class="metric-text">
+			<p>Number of new Investments</p>
+			</div>
+			<div class="metric-amount1 no_change">
+			<p>00</p>			
+			</div>
+			</article>       
+			</div>
+		</div>	
+			<div class="fundbtn"><a href="#" class="btn disabled" id="dashbtn_fix">Detailed View</a></div>	
+        </div>
+
+""", MAGNATE_FUNDS, dict(
+label = "Template for statistics for %s" % (fund.name),
+help_text = """
+Template for statistics for %s 
+""" % (fund.name),
+widget=Textarea(attrs={'rows': '20'})))
+	MAGNATE_FUND_TEMPLATES[fund.pk] = FUND_METRICS_TEMPLATE
+
