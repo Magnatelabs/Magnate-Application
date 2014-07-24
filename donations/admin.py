2c8f0a6a85ac1911bbb7d178c538b355c8e7560e
Sergey Orshanskiy <orshansk@gmail.com>
2014-07-23 23:03:19 -0400

    Added MagnateFund, PortfolioCompany to admin

--- donations/admin.py
+++ donations/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from donations.models import Donation
+from donations.models import Donation, MagnateFund, PortfolioCompany
 
 admin.site.register(Donation)
+admin.site.register(MagnateFund)
+admin.site.register(PortfolioCompany)
 
