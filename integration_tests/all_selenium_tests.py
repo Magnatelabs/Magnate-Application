74cb7c8f4d730ee5aa7c666ea44233e393cbb7b2
Sergey Orshanskiy <orshansk@gmail.com>
2014-08-17 03:57:44 -0400

    1.scroll_and_click: scrolling to make elements visible before clicking on them, otherwise it does not work; 2.now trying to vote for the question we have just asked, and clicking OK on the dialog that we cannot vote for our own question

--- integration_tests/all_selenium_tests.py
+++ integration_tests/all_selenium_tests.py
@@ -56,18 +56,34 @@ class main_page(MyTestCase):
         # Make sure we are on the dashboard
         self.by_class_name("user_breakdown")
 
+        # The firefox driver for selenium has a problem
+        # When we are simulating a click on an element
+        # that is not in the current viewport, it is
+        # not working. Even if the element is under the
+        # navbar, it is still not working, even though
+        # it is technically visible and enabled.
+        # 
+        # So scroll so the element will be right under
+        # the navbar.
+        navbar_height=self.by_class_name('navbar-inner').size['height']
+        def scroll_and_click(elt):
+            self.driver.execute_script('window.scrollTo(0,%d);' % (elt.location['y'] - navbar_height) )
+            elt.click()
+
         # We've just created a public entry; find it in the feed!
         entry = self.by_id('entry-1')
 
         # Let's expand it so we can see the standard picture, the (empty) list of Q&A, etc.
 
-        entry.click()
-        self.by_link_text('Ask Question').click()
+        scroll_and_click ( entry )
+
+        scroll_and_click ( self.by_link_text('Ask Question') )
 
         self.by_id('id_title').send_keys("Original question")
         self.by_id('editor').send_keys("I am just curious about this Magnate thing. Does it work?")
         self.by_id('id_tags').send_keys("user question curious")
-        self.by_name('ask').click()
+
+        scroll_and_click ( self.by_name('ask') )
 
         # Assumption: the question was asked successfully, we were redirected
         self.assertIn('original-question', self.driver.current_url)
@@ -82,19 +98,25 @@ class main_page(MyTestCase):
         # Once again select the first entry!
         entry = self.by_id('entry-1')
         # Select it once again; see if we can see our question on the right
-        entry.click()
+        
+        scroll_and_click( entry )
 
         # Now very important; see if we see this question we've just asked!
         # It should be shown after we've clicked once again on the entry
         link=self.by_partial_link_text('Original question')
 
-        # let's open it
-        link.click()
+        scroll_and_click(link)
+
+        # try to vote for the question, up or down (whichever comes first, probably up)
+        scroll_and_click( self.by_class_name('post-vote') )
+        # it should say we can't vote for our own questions; click 'ok'        
+        scroll_and_click( self.by_class_name('dialog-yes') )
+
         editor=self.by_id('editor')
         editor.send_keys("I am trying to answer my own question. Please, be kind...")
         # not a great way to locate the submit button
         # better to give it some id..
-        self.by_class_name('submit').click()
+        scroll_and_click( self.by_class_name('submit') )
 
         # Make sure we still see the dashboard
         self.by_class_name("user_breakdown")
