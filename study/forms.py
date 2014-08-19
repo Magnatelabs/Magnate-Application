0b1214d4d084d8a20429baf8323212d765a282e9
Sergey Orshanskiy <orshansk@gmail.com>
2014-08-19 13:41:41 -0400

    docfile (file with additional data) is no longer required! Also now using forms.URLField, forms.ChoiceField, widgets.

--- study/forms.py
+++ study/forms.py
@@ -2,13 +2,14 @@ from django import forms
 from .models import StudyModel
 from django.utils.safestring import mark_safe
 
+INDUSTRY_CHOICES = (('mobile', 'Mobile'), ('e-commerce', 'E-commerce'), ('social-network', 'Social Network'), ('fitness', 'Fitness'), ('other', 'Other'))
 
 class StudyModelForm(forms.Form):
     entity = forms.CharField(max_length=255)
-    entity_url = forms.CharField(max_length=255)
-    description = forms.CharField(max_length=750)
-    industry = forms.CharField(max_length=255)
-    docfile = forms.FileField(label='Additional Material',)
+    entity_url = forms.URLField(max_length=255)
+    description = forms.CharField(widget=forms.Textarea, max_length=750)
+    industry = forms.ChoiceField(widget=forms.Select, choices=INDUSTRY_CHOICES)
+    docfile = forms.FileField(label='Additional Material', required=False)
  
     def __init__(self, *args, **kwargs):
         super(StudyModelForm, self).__init__(*args, **kwargs)
