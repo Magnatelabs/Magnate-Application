6fc24de69f44d6bfe54872270ae71db76b14e547
Sergey Orshanskiy <orshansk@gmail.com>
2014-09-26 22:59:14 -0400

    entry_feed now also has parameter highlighted; set it to True for a feed of highlighted entries (entries will be passed highlighted=True in the template)

--- dashboard/templatetags/magnate_tags.py
+++ dashboard/templatetags/magnate_tags.py
@@ -3,8 +3,9 @@ from django import template
 register=template.Library()
 
 @register.inclusion_tag('dashboard/_entry_feed.html')
-def entry_feed(entries, pagination=True):
+def entry_feed(entries, pagination=True, highlighted=False):
 	return {
 		'entries': entries,
 		'pagination': pagination,
+		'highlighted': highlighted
 	}
