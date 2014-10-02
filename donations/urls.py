8811a571c96e3918f0793a53a79af9f61491b71e
jmwilliams(air) <jolumwilliams@gmail.com>
2014-10-02 11:38:48 -0700

    Added the new template page to URLS, and removed the [donations] in all other urls

--- donations/urls.py
+++ donations/urls.py
@@ -12,11 +12,12 @@ from  donations.views import DonationBilling
 urlpatterns = patterns("",
 #  url(r"^donations/home$", "donations.views.donation_index", name="donations_home"),
   url(r"^home$", "donations.views.fundpage_index", name="fund_home"),
-  url(r"^donations/add$", "donations.views.donation_add", name="donations_add"),
-  url(r"^donations/billing$", "donations.views.enter_billing_info", name="donations_billing"),
-  url(r"^donations/finalize_order$", DonationBilling.as_view(), name="donations_finalize"),
-  url(r"^donations/confirmation$", "donations.views.donation_confirmation", name="donations_confirmation"),
-  url(r"^donations/order_pay$", "donations.views.donation_orderpay", name="donations_pay"),
+  url(r"^add$", "donations.views.donation_add", name="donations_add"),
+  url(r"^billing$", "donations.views.enter_billing_info", name="donations_billing"),
+  url(r"^finalize_order$", DonationBilling.as_view(), name="donations_finalize"),
+  url(r"^confirmation$", "donations.views.donation_confirmation", name="donations_confirmation"),
+  url(r"^order_pay$", "donations.views.donation_orderpay", name="donations_pay"),
+  url(r"^user$", "donations.views.contribution_history", name="contribution_user"),
 )
 
 int_obj = get_integration("authorize_net_dpm")
