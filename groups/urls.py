f8c126020755f93f3e862e51301c29127fb6be50
jmwilliams(air) <jolumwilliams@gmail.com>
2014-10-24 11:24:02 -0700

    Changed the info URL to display category number and added descriptions behind groups

--- groups/urls.py
+++ groups/urls.py
@@ -7,8 +7,8 @@ admin.autodiscover()
 
 urlpatterns = patterns("",
   url(r"^all/$", "groups.views.groups_index", name="groups_all"),
-  url(r"^(?P<id>\d+)/$", "groups.views.groups_detail_home", name="category_home_page"),
-  url(r"^info/$", "groups.views.groups_detail_information", name="groups_detail_info"),
+  url(r"^(?P<id>\d+)/home/$", "groups.views.groups_detail_home", name="category_home_page"),
+  url(r"^(?P<id>\d+)/info/$", "groups.views.groups_detail_information", name="groups_detail_info"),
   url(r"^current_objective/$", "groups.views.groups_detail_objective", name="groups_detail_objective"),
 
   url(r'^follow_category/(?P<id>\d+)/$', "groups.views.ajax_follow_category", name='follow_category'),
