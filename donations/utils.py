c992038957e649a3ecd545e8401047dc91926aa1
jmwilliams(air) <jolumwilliams@gmail.com>
2014-11-11 15:13:34 -0800

    Added new utlity function and refactor lines with asserts that do not currently return the same result

--- donations/utils.py
+++ donations/utils.py
@@ -1,14 +1,19 @@
 from .models import Donation
 from django.conf import settings
 import datetime
+from django.db.models import Sum
 
 # username is a string, such as 'admin'
 # Returns a Decimal
 def total_donation_amount(username):
-    return sum([d.amount for d in Donation.objects.filter(user__username=username)])
+    result = sum([d.amount for d in Donation.objects.filter(user__username=username)])
+#    assert result == Donation.objects.filter(user__username=username).aggregate(total_donation_amount=Sum('amount')) ['total_donation_amount']
+    return result 
 
 def all_donations_by_user(username):
     return Donation.objects.filter(user__username=username)
 
-def donation_vesting_date(username):
-    return datetime.datetime(2015,1,1)
+def no_objective_total(username):
+    result = sum([d.amount for d in Donation.objects.filter(user__username=username, objective__isnull=True)])
+  #  assert result == Donation.objects.filter(user__username=username, objective__isnull=True).aggregate(amount_no_objective=Sum('amount'))['amount_no_objective']
+    return result
