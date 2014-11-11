1532ae422d623157836c113ac2da9f39f33c0cf7
jmwilliams(air) <jolumwilliams@gmail.com>
2014-11-11 15:35:46 -0800

    Returned DEBUG to True because we have not set hosts yet

--- app_magnate/settings.py
+++ app_magnate/settings.py
@@ -4,7 +4,7 @@ import os
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 PACKAGE_ROOT = os.path.abspath(os.path.dirname(__file__))
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = [
